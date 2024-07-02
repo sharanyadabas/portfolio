69d9b74d05d9228f5deba9855fb214926109d1e2
sharanyadabas <sd699@cornell.edu>
2024-07-02 18:04:56 -0400

    Finished animations for projects page

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -4,6 +4,7 @@ import ProjectCard from "../components/ProjectCard";
 import ThemeBox from "../components/ThemeBox";
 import Stack from "@mui/material/Stack";
 import IconButton from "@mui/material/IconButton";
+import { motion } from "framer-motion";
 
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
@@ -51,10 +52,37 @@ const allProjects = [
   ExtraProject2,
 ];
 
+const mainCardVariants = {
+  hidden: { opacity: 0, x: 50 },
+  visible: { opacity: 1, x: 0 },
+  exit: { opacity: 0, x: -100 },
+};
+
+const smallCardVariants = {
+  // Enter from the right
+  enter: (direction) => ({
+    x: direction > 0 ? 100 : -100, // Enter from left or right based on direction
+    opacity: 0,
+  }),
+  // Center position with full opacity
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  // Exit to the left
+  exit: (direction) => ({
+    zIndex: 0,
+    x: direction < 0 ? 100 : -100, // Exit to left or right based on direction
+    opacity: 0,
+  }),
+};
+
 function Projects() {
   const [pointers, setPointers] = React.useState({ left: 0, right: 3 });
   const [projects, setProjects] = React.useState([]);
-  const [mainCard, setMainCard] = React.useState(allProjects[1]);
+  const [mainCard, setMainCard] = React.useState(allProjects[0]);
+  const [direction, setDirection] = React.useState(0);
 
   const sliceWrap = (array, start, end) => {
     if (start <= end) {
@@ -82,6 +110,7 @@ function Projects() {
     const leftPointer = (pointers.left + 1) % allProjects.length;
     const rightPointer = (pointers.right + 1) % allProjects.length;
     setPointers({ left: leftPointer, right: rightPointer });
+    setDirection(1);
   };
   const shiftProjectsLeft = () => {
     const leftPointer =
@@ -89,6 +118,7 @@ function Projects() {
     const rightPointer =
       pointers.right === 0 ? allProjects.length - 1 : pointers.right - 1;
     setPointers({ left: leftPointer, right: rightPointer });
+    setDirection(-1);
   };
 
   return (
@@ -103,7 +133,16 @@ function Projects() {
         gap="5vh"
       >
         <Stack direction="row" alignItems="center" gap="3vw">
-          <MainProjectCard {...mainCard} />
+          <motion.div
+            key={mainCard.title}
+            variants={mainCardVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            transition={{ type: "spring", stiffness: 30, damping: 10 }}
+          >
+            <MainProjectCard {...mainCard} />
+          </motion.div>
         </Stack>
         <Stack direction="row" spacing={7} alignItems="center">
           <IconButton
@@ -113,13 +152,22 @@ function Projects() {
           >
             <KeyboardArrowLeftIcon sx={{ fontSize: 70 }} />
           </IconButton>
-          
-          {projects.map((project) => (
-            <ProjectCard
-              key={project.title}
-              {...project}
-              onClick={() => changeMainCard(project)}
-            />
+          {projects.map((project, index) => (
+            <motion.div
+              key={index + project.title}
+              custom={direction}
+              variants={smallCardVariants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{ type: "spring", stiffness: 60, damping: 10 }}
+            >
+              <ProjectCard
+                key={project.title}
+                {...project}
+                onClick={() => changeMainCard(project)}
+              />
+            </motion.div>
           ))}
           <IconButton
             color="inherit"
