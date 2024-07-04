78143458437002923cce2b12c17284102234f2c9
sharanyadabas <sd699@cornell.edu>
2024-07-04 01:10:22 -0400

    Added zoom animation to contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
 import ThemeBox from "../components/ThemeBox";
 import ContactCard from "../components/ContactCard";
+import Zoom from "@mui/material/Zoom";
 
 function Contact() {
+  const [checked, setChecked] = React.useState(false);
+
+  React.useEffect(() => {
+    setChecked(true);
+  }, []);
+
   return (
     <div style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
       <ResponsiveAppBar />
@@ -15,16 +22,20 @@ function Contact() {
         flexGrow="1"
         gap="5vh"
       >
-        <ContactCard
-          name={"Sharanya Dabas"}
-          description={"Aspiring Software Engineer"}
-          headerImage={"/portfolio/images/cornell-banner.jpg"}
-          image={"/portfolio/images/headshot.jpg"}
-          linkEmail={"sd699@cornell.edu"}
-          linkLinkedin={"https://www.linkedin.com/in/sharanya-dabas/"}
-          linkGithub={"https://github.com/sharanyadabas"}
-          phoneNumber={"(781) 985-1259"}
-        />
+        <Zoom in={checked} timeout={{ enter: 500 }}>
+          <div>
+            <ContactCard
+              name={"Sharanya Dabas"}
+              description={"Aspiring Software Engineer"}
+              headerImage={"/portfolio/images/cornell-banner.jpg"}
+              image={"/portfolio/images/headshot.jpg"}
+              linkEmail={"sd699@cornell.edu"}
+              linkLinkedin={"https://www.linkedin.com/in/sharanya-dabas/"}
+              linkGithub={"https://github.com/sharanyadabas"}
+              phoneNumber={"(781) 985-1259"}
+            />
+          </div>
+        </Zoom>
       </ThemeBox>
     </div>
   );
