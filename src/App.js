0903745d1e549fea36a21126bdd1ff882c245655
sharanyadabas <sd699@cornell.edu>
2024-07-08 02:00:49 -0400

    Changed to hashrouter because of issue with github pges

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import ThemeContext from "./ThemeContext";
 
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
   Navigate,
