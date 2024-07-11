1979d8b65b5ed1aad8f1d1c7f3f35bba0dd99d7e
Sharanya Dabas <sdabas3001@gmail.com>
2024-07-11 02:43:29 -0400

    Organized imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { useState } from "react";
 import "./App.css";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import ThemeContext from "./ThemeContext";
