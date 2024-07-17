1f2ddb5411ec9948fbcd7b3aeb14215a1985dfba
Sharanya Dabas <sdabas3001@gmail.com>
2024-07-17 14:43:28 -0400

    Changed Exercise Engine URL due to AWS update

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -17,7 +17,7 @@ const ExerciseEngine = {
   description:
     "Exercise search engine that is powered by single value decomposition on exercise descriptions to rank similarities between different exercises. Users can query via dropdown or ad-hoc search and use filters on muscle groups or equipment.",
   image: "/images/exengine.jpeg",
-  link: "http://3.140.251.0:8080/",
+  link: "http://3.15.240.235:8080/",
   linkgithub: "https://github.com/sharanyadabas/Exercise-Engine-SVD",
   technologies: [
     "python",
